f771edc1c3e93fadf707fbbd9686492dae832e8b
brunosm16 <brunoskr23@gmail.com>
2023-04-25 00:27:12 -0300

    refactor: 📝 use default error message

--- src/services/users/validate-user-service.js
+++ src/services/users/validate-user-service.js
@@ -1,12 +1,12 @@
-const { ENTITY_EXISTS, TOKEN_ERROR } = require('../../constants/error-messages');
-const { EntityExistsError } = require('../../errors/entity-exists');
+const { TOKEN_ERROR } = require('../../constants/error-messages');
+const { EntityExistsError } = require('../../errors/entity-exists-error');
 const { InternalServerError } = require('../../errors/internal-server-error');
 const findUserService = require('./find-user-service');
 
 const validateUserExists = async ({ first_name, last_name, primary_email }) => {
 	try {
 		const user = await findUserService.findUserByParams(first_name, last_name, primary_email);
-		if (user) throw new EntityExistsError(ENTITY_EXISTS);
+		if (user) throw new EntityExistsError();
 	} catch (err) {
 		throw new InternalServerError(err?.message);
 	}
