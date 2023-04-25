a1f6f0f802f0ef19398dae475daec5e9154b3403
brunosm16 <brunoskr23@gmail.com>
2023-04-25 00:18:15 -0300

    refactor: 📝 reuse code to send error

--- src/http/http-responses.js
+++ src/http/http-responses.js
@@ -1,7 +1,25 @@
+const { EntityExistsError } = require('../errors/entity-exists');
+const { NotFoundError } = require('../errors/not-found-error');
+
 const sendResponse = (res, statusCode, message) => res.status(statusCode).json({ message });
 
 const makeOkResponse = (res, message) => sendResponse(res, 200, message);
 const makeBadRequestResponse = (res, message) => sendResponse(res, 400, message);
 const makeInternalServerErrorResponse = (res, message) => sendResponse(res, 500, message);
 
-module.exports = { makeOkResponse, makeBadRequestResponse, makeInternalServerErrorResponse };
+const makeResponseByError = (res, err) => {
+	const { message } = err;
+
+	if (err instanceof EntityExistsError || err instanceof NotFoundError) {
+		return makeBadRequestResponse(res, message);
+	}
+
+	return makeInternalServerErrorResponse(res, message);
+};
+
+module.exports = {
+	makeOkResponse,
+	makeBadRequestResponse,
+	makeInternalServerErrorResponse,
+	makeResponseByError,
+};
