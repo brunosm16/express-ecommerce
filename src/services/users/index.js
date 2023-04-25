c88f31ee7a56083516cfa17aa4d68f78408d304c
brunosm16 <brunoskr23@gmail.com>
2023-04-24 23:48:06 -0300

    feat: 🚀 exports services

---
+++ src/services/users/index.js
@@ -0,0 +1,4 @@
+const createUserService = require('./create-user-service');
+const findUserService = require('./find-user-service');
+
+module.exports = { createUserService, findUserService };
--- src/utils/send-express-response.js
+++
@@ -1,4 +0,0 @@
-const sendExpressResponse = (res, { statusCode, message }) =>
-	res.status(statusCode).json({ message });
-
-module.exports = { sendExpressResponse };
