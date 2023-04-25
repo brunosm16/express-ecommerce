0612a32497340019ffc612e0244644090ec96942
brunosm16 <brunoskr23@gmail.com>
2023-04-25 10:54:13 -0300

    refactor: 📝 improve error treatment on users service

--- src/services/users/find-user-service.js
+++ src/services/users/find-user-service.js
@@ -1,21 +1,25 @@
-const { makeInternalServerError, makeOkRequest } = require('../../errors/errors-factory');
+const { InternalServerError } = require('../../errors');
 const UserModel = require('../../models/UserModel');
 
 const findUserByParams = async (first_name, last_name, primary_email) => {
-	return UserModel.findOne({ where: { first_name, last_name, primary_email } });
-};
-
-const findAllUsers = async () => {
 	try {
-		const users = await UserModel.findAll({ attributes: { exclude: ['password_hash'] } });
-
-		return makeOkRequest(users);
+		return await UserModel.findOne({ where: { first_name, last_name, primary_email } });
 	} catch (err) {
-		return makeInternalServerError(err?.message);
+		throw new InternalServerError(err?.message);
 	}
 };
 
+// const findAllUsers = async () => {
+// 	try {
+// 		const users = await UserModel.findAll({ attributes: { exclude: ['password_hash'] } });
+
+// 		return makeOkRequest(users);
+// 	} catch (err) {
+// 		return makeInternalServerError(err?.message);
+// 	}
+// };
+
 module.exports = {
 	findUserByParams,
-	findAllUsers,
+	// findAllUsers,
 };
--- src/services/users/validate-user-service.js
+++ src/services/users/validate-user-service.js
@@ -3,18 +3,12 @@ const { EntityExistsError, InternalServerError } = require('../../errors');
 const findUserService = require('./find-user-service');
 
 const validateUserExists = async ({ first_name, last_name, primary_email }) => {
-	try {
-		const user = await findUserService.findUserByParams(first_name, last_name, primary_email);
-		if (user) throw new EntityExistsError();
-	} catch (err) {
-		throw new InternalServerError(err?.message);
-	}
+	const user = await findUserService.findUserByParams(first_name, last_name, primary_email);
+	if (user) throw new EntityExistsError();
 };
 
 const validateToken = (token) => {
-	if (!token) {
-		throw new InternalServerError(TOKEN_ERROR);
-	}
+	if (!token) throw new InternalServerError(TOKEN_ERROR);
 };
 
 module.exports = { validateUserExists, validateToken };
