e87bacf8abf6cb96d621eb9e5a5a0261234ca4f2
brunosm16 <brunoskr23@gmail.com>
2023-04-25 13:29:59 -0300

    refactor: 📝 reuse code on http-response

--- src/http/http-responses.js
+++ src/http/http-responses.js
@@ -8,17 +8,20 @@ const makeBadRequestResponse = (res, message) => sendResponse(res, 400, message)
 const makeInternalServerErrorResponse = (res, message) => sendResponse(res, 500, message);
 
 const isTypeError = (err) => err?.stack?.startsWith('TypeError');
+const isTokenExpiredError = (err) => err?.name === 'TokenExpiredError';
+
+const isBadRequestError = (err) => {
+	const badRequestErrors = [EntityExistsError, NotFoundError];
+
+	return badRequestErrors.some((errorType) => err instanceof errorType);
+};
 
 const makeResponseByError = (res, err) => {
 	const { message } = err;
 
-	if (isTypeError(err)) {
-		return makeInternalServerErrorResponse(res, INTERNAL_SERVER_ERROR);
-	}
-
-	if (err instanceof EntityExistsError || err instanceof NotFoundError) {
-		return makeBadRequestResponse(res, message);
-	}
+	if (isTypeError(err)) return makeInternalServerErrorResponse(res, INTERNAL_SERVER_ERROR);
+	if (isTokenExpiredError) return makeBadRequestResponse(res, message);
+	if (isBadRequestError(err)) return makeBadRequestResponse(res, message);
 
 	return makeInternalServerErrorResponse(res, err?.message);
 };
