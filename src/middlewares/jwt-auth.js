d680465ef99970d404ac52a8e6ca6d1062bab828
brunosm16 <brunoskr23@gmail.com>
2023-04-25 09:58:47 -0300

    refactor: 📝 reuse code on authentication middleware

--- src/middlewares/jwt-auth.js
+++ src/middlewares/jwt-auth.js
@@ -1,27 +1,7 @@
-const {
-	REQUIRED_AUTHORIZATION_ERROR,
-	INVALID_TOKEN_CREDENTIAL,
-	NOT_ALLOWED,
-} = require('../constants/error-messages');
-const { makeBadRequest, makeInternalServerError } = require('../errors/errors-factory');
+const { NotAllowedError } = require('../errors');
+const { makeResponseByError } = require('../http/http-responses');
+const { validateAuthorization } = require('../services/auth/auth-service');
 const { decodeToken } = require('../services/cryptography/cryptography-service');
-const { sendExpressResponse } = require('../utils/send-express-response');
-
-const authIsNotBearer = (authorization) => {
-	const split = authorization.split(' ');
-
-	if (split.length !== 2 && split[0] !== 'Bearer') return true;
-
-	return false;
-};
-
-const getAuthError = (authorization) => {
-	if (!authorization) return makeBadRequest(REQUIRED_AUTHORIZATION_ERROR);
-
-	if (authIsNotBearer(authorization)) return makeBadRequest(INVALID_TOKEN_CREDENTIAL);
-
-	return null;
-};
 
 const getBearerTokenDecoded = (authorization) => {
 	const bearerToken = authorization.split(' ')[1];
@@ -30,31 +10,31 @@ const getBearerTokenDecoded = (authorization) => {
 
 const adminAuth = async (req, res, next) => {
 	const { authorization } = req.headers;
-	const authError = getAuthError(authorization);
-	if (authError) return sendExpressResponse(res, authError);
+
+	validateAuthorization(authorization);
 
 	try {
 		const { admin } = getBearerTokenDecoded(authorization);
 
-		if (!admin) sendExpressResponse(res, makeBadRequest(NOT_ALLOWED));
+		if (!admin) throw new NotAllowedError();
 
 		return next();
 	} catch (err) {
-		return sendExpressResponse(res, makeInternalServerError(err?.message));
+		return makeResponseByError(err);
 	}
 };
 
 const auth = async (req, res, next) => {
-	const { authorization } = req.headers;
-	const authError = getAuthError(authorization);
-	if (authError) return sendExpressResponse(res, authError);
-
 	try {
-		const { admin } = getBearerTokenDecoded();
+		const { authorization } = req.headers;
+
+		validateAuthorization(authorization);
+
+		const { admin } = getBearerTokenDecoded(authorization);
 		req.isAdmin = admin;
 		return next();
 	} catch (err) {
-		return sendExpressResponse(res, makeBadRequest(INVALID_TOKEN_CREDENTIAL));
+		return makeResponseByError(res, err);
 	}
 };
 
---
+++ src/services/auth/auth-service.js
@@ -0,0 +1,19 @@
+const { INVALID_TOKEN_CREDENTIAL } = require('../../constants/error-messages');
+const { RequiredFieldError, InvalidCredentialsError } = require('../../errors');
+
+const authorizationIsNotBearer = (authorization) => {
+	const split = authorization.split(' ');
+
+	if (split.length !== 2 && split[0] !== 'Bearer') return true;
+
+	return false;
+};
+
+const validateAuthorization = (authorization) => {
+	if (!authorization) throw new RequiredFieldError('Authorization is a required field');
+
+	if (authorizationIsNotBearer(authorization))
+		throw new InvalidCredentialsError(INVALID_TOKEN_CREDENTIAL);
+};
+
+module.exports = { validateAuthorization };
