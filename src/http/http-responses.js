ce108b74206ae9281b30eff5d1fa142b2eaade8d
brunosm16 <brunoskr23@gmail.com>
2023-04-25 10:51:50 -0300

    fix: 🚧 add type-error treatment

--- src/http/http-responses.js
+++ src/http/http-responses.js
@@ -1,3 +1,4 @@
+const { INTERNAL_SERVER_ERROR } = require('../constants/error-messages');
 const { EntityExistsError, NotFoundError } = require('../errors');
 
 const sendResponse = (res, statusCode, message) => res.status(statusCode).json({ message });
@@ -6,14 +7,20 @@ const makeOkResponse = (res, message) => sendResponse(res, 200, message);
 const makeBadRequestResponse = (res, message) => sendResponse(res, 400, message);
 const makeInternalServerErrorResponse = (res, message) => sendResponse(res, 500, message);
 
+const isTypeError = (err) => err?.stack?.startsWith('TypeError');
+
 const makeResponseByError = (res, err) => {
 	const { message } = err;
+
+	if (isTypeError(err)) {
+		return makeInternalServerErrorResponse(res, INTERNAL_SERVER_ERROR);
+	}
 
 	if (err instanceof EntityExistsError || err instanceof NotFoundError) {
 		return makeBadRequestResponse(res, message);
 	}
 
-	return makeInternalServerErrorResponse(res, message);
+	return makeInternalServerErrorResponse(res, err?.message);
 };
 
 module.exports = {
