38aac8d410dc3e2719ab1cba985c384c81f0df17
brunosm16 <brunoskr23@gmail.com>
2023-04-25 22:51:47 -0300

    refactor: 📝 use status-code and improve error treatment

--- src/http/http-responses.js
+++ src/http/http-responses.js
@@ -1,29 +1,20 @@
-const { INTERNAL_SERVER_ERROR } = require('../constants/error-messages');
-const { EntityExistsError, NotFoundError } = require('../errors/errors-types');
+const {
+	STATUS_CODE_200,
+	STATUS_CODE_400,
+	STATUS_CODE_500,
+} = require('../constants/http-status-codes');
+const { createError } = require('../errors/errors-helpers');
 
 const sendResponse = (res, statusCode, message) => res.status(statusCode).json({ message });
-
-const makeOkResponse = (res, message) => sendResponse(res, 200, message);
-const makeBadRequestResponse = (res, message) => sendResponse(res, 400, message);
-const makeInternalServerErrorResponse = (res, message) => sendResponse(res, 500, message);
-
-const isTypeError = (err) => err?.stack?.startsWith('TypeError');
-const isTokenExpiredError = (err) => err?.name === 'TokenExpiredError';
-
-const isBadRequestError = (err) => {
-	const badRequestErrors = [EntityExistsError, NotFoundError];
-
-	return badRequestErrors.some((errorType) => err instanceof errorType);
-};
+const makeOkResponse = (res, message) => sendResponse(res, STATUS_CODE_200, message);
+const makeBadRequestResponse = (res, message) => sendResponse(res, STATUS_CODE_400, message);
+const makeInternalServerErrorResponse = (res, message) =>
+	sendResponse(res, STATUS_CODE_500, message);
 
 const makeResponseByError = (res, err) => {
-	const { message } = err;
+	const { statusCode, message } = createError(err);
 
-	if (isTypeError(err)) return makeInternalServerErrorResponse(res, INTERNAL_SERVER_ERROR);
-	if (isTokenExpiredError) return makeBadRequestResponse(res, message);
-	if (isBadRequestError(err)) return makeBadRequestResponse(res, message);
-
-	return makeInternalServerErrorResponse(res, err?.message);
+	return sendResponse(res, statusCode, message);
 };
 
 module.exports = {
