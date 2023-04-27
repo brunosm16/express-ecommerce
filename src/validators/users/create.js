054462ebac3e2803546ea18a0ce70a84e2a4a776
brunosm16 <brunoskr23@gmail.com>
2023-04-27 10:54:36 -0300

    feat: 🚀 make sures primary_email is different from secondary

--- src/validators/users/create.js
+++ src/validators/users/create.js
@@ -5,7 +5,9 @@ module.exports = celebrate({
 		first_name: Joi.string().required(),
 		last_name: Joi.string().required(),
 		primary_email: Joi.string().email().required(),
-		secondary_email: Joi.string().email().required(),
+		secondary_email: Joi.string().email().disallow(Joi.ref('primary_email')).required().messages({
+			'any.invalid': 'Secondary email must be different from primary_email',
+		}),
 		date_of_birth: Joi.string().isoDate().required(),
 		password: Joi.string().required().min(8),
 		admin: Joi.boolean().required(),
