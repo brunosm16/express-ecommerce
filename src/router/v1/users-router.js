9bcf1d6077013255e864d3a61b1f6b66e3f9e5c8
brunosm16 <brunoskr23@gmail.com>
2023-04-27 16:23:36 -0300

    refactor: 📝 routes authorization

--- src/router/v1/users-router.js
+++ src/router/v1/users-router.js
@@ -5,11 +5,11 @@ const usersController = require('../../controllers/users');
 const userValidator = require('../../validators/users');
 const { adminAuth } = require('../../middlewares/jwt-auth');
 
-usersRouter.get('/', userValidator.findAll, usersController.findAll);
+usersRouter.get('/', userValidator.findAll, adminAuth, usersController.findAll);
+
+usersRouter.post('/', userValidator.create, adminAuth, usersController.create);
 
 usersRouter.post('/login', userValidator.login, usersController.login);
-
-usersRouter.post('/', userValidator.create, usersController.create);
 
 usersRouter.delete('/:id', userValidator.deleteById, adminAuth, usersController.deleteById);
 
