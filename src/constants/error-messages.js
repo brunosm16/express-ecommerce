a5d7b15f8be364afaa33af04fbb27c8197980ba5
brunosm16 <brunoskr23@gmail.com>
2023-04-27 10:36:01 -0300

    feat: 🚀 add new error-messages

--- src/constants/error-messages.js
+++ src/constants/error-messages.js
@@ -2,8 +2,11 @@ module.exports = Object.freeze({
 	TOKEN_ERROR:
 		'Operation not finished, an error occurred while creating user token. Please try again later',
 	ENTITY_EXISTS: 'Entity already exists',
+	ENTITY_NOT_EXISTS: 'Entity not exists',
 	REQUIRED_AUTHORIZATION_ERROR: 'Authorization is required',
 	INVALID_TOKEN_CREDENTIAL: 'Invalid token credential',
 	INTERNAL_SERVER_ERROR: 'Internal Server Error',
+	SOMETHING_WRONG: 'Something wrong happened. Please try again later',
+	ENTITY_DELETED: 'Entity was successfully deleted',
 	NOT_ALLOWED: 'You are not allowed to access this information',
 });
