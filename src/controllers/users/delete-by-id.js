8ce0ca9c81af8fab20c8d18f48d6d9ab0b8afb90
brunosm16 <brunoskr23@gmail.com>
2023-05-02 11:11:36 -0300

    refactor: 📝 reuse function on delete-by-id

--- src/controllers/users/delete-by-id.js
+++ src/controllers/users/delete-by-id.js
@@ -1,18 +1,5 @@
-const { SOMETHING_WRONG, ENTITY_DELETED } = require('../../constants/error-messages');
-const {
-	makeOkResponse,
-	makeResponseByError,
-	makeInternalServerErrorResponse,
-} = require('../../http/http-responses');
+const { makeResponseByError, makeDeleteResponse } = require('../../http/http-responses');
 const deleteUserService = require('../../services/users/delete-user-service');
-
-const OPERATION_ERROR_CODE = 0;
-const OPERATION_ERROR_SUCCESS = 1;
-
-const responsesByCode = {
-	[OPERATION_ERROR_CODE]: (res) => makeInternalServerErrorResponse(res, SOMETHING_WRONG),
-	[OPERATION_ERROR_SUCCESS]: (res) => makeOkResponse(res, ENTITY_DELETED),
-};
 
 module.exports = async (req, res) => {
 	try {
@@ -20,7 +7,7 @@ module.exports = async (req, res) => {
 
 		const deletedUser = await deleteUserService.deleteUser(id);
 
-		return responsesByCode[deletedUser](res);
+		return makeDeleteResponse(deletedUser, res);
 	} catch (err) {
 		return makeResponseByError(res, err);
 	}
