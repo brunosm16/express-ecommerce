517ecd5f1ffaa20fdf7d2067c12c81d2d8dd9766
brunosm16 <brunoskr23@gmail.com>
2023-05-02 09:56:08 -0300

    feat: 🚀 add authorization on addresses router

--- src/router/v1/addresses-router.js
+++ src/router/v1/addresses-router.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const addressesValidator = require('../../validators/addresses');
 const addressesController = require('../../controllers/addresses');
+const { auth, adminAuth } = require('../../middlewares/jwt-auth');
 
 const addressesRouter = express.Router();
 
-addressesRouter.get('/', addressesValidator.findAll, addressesController.findAll);
-addressesRouter.post('/', addressesValidator.create, addressesController.create);
+addressesRouter.get('/', addressesValidator.findAll, auth, addressesController.findAll);
+addressesRouter.post('/', addressesValidator.create, adminAuth, addressesController.create);
 
 module.exports = addressesRouter;
