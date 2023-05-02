825baeff691cb392686d48ec1beda796bb4140de
brunosm16 <brunoskr23@gmail.com>
2023-05-02 11:34:54 -0300

    feat: 🚀 validate address before returning by id

--- src/services/addresses/find-address-service.js
+++ src/services/addresses/find-address-service.js
@@ -1,10 +1,12 @@
 const AddressModel = require('../../models/AddressModel');
+const { validateEntityNotExistsByPk } = require('../entities/validate-entity');
 
 const findAll = () => {
 	return AddressModel.findAll();
 };
 
-const findById = (id) => {
+const findById = async (id) => {
+	await validateEntityNotExistsByPk(id, AddressModel);
 	return AddressModel.findByPk(id);
 };
 
