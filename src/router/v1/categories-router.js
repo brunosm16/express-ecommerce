6b93c62ba8cb4fb732e5bb3baa8afad10b7855a4
brunosm16 <brunoskr23@gmail.com>
2023-05-03 09:13:40 -0300

    feat: 🚀 add authentication to categories route

--- src/router/v1/categories-router.js
+++ src/router/v1/categories-router.js
@@ -1,13 +1,24 @@
 const express = require('express');
 const categoriesValidator = require('../../validators/categories');
 const categoriesController = require('../../controllers/categories');
+const { adminAuth } = require('../../middlewares/jwt-auth');
 
 const categoriesRouter = express.Router();
 
-categoriesRouter.post('/', categoriesValidator.create, categoriesController.create);
-categoriesRouter.delete('/:id', categoriesValidator.deleteById, categoriesController.deleteById);
-categoriesRouter.get('/', categoriesValidator.findAll, categoriesController.findAll);
-categoriesRouter.get('/:id', categoriesValidator.findById, categoriesController.findById);
-categoriesRouter.put('/:id', categoriesValidator.update, categoriesController.update);
+categoriesRouter.post('/', categoriesValidator.create, adminAuth, categoriesController.create);
+categoriesRouter.delete(
+	'/:id',
+	categoriesValidator.deleteById,
+	adminAuth,
+	categoriesController.deleteById
+);
+categoriesRouter.get('/', categoriesValidator.findAll, adminAuth, categoriesController.findAll);
+categoriesRouter.get(
+	'/:id',
+	categoriesValidator.findById,
+	adminAuth,
+	categoriesController.findById
+);
+categoriesRouter.put('/:id', categoriesValidator.update, adminAuth, categoriesController.update);
 
 module.exports = categoriesRouter;
