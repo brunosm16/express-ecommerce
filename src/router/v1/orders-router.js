831580dc51106df2a9ff6e8a6b68219e32b71125
brunosm16 <brunoskr23@gmail.com>
2023-05-10 09:24:27 -0300

    feat: 🚀 add admin auth middleware for orders

--- src/router/v1/orders-router.js
+++ src/router/v1/orders-router.js
@@ -1,11 +1,12 @@
 const express = require('express');
+const { adminAuth } = require('../../middlewares/jwt-auth');
 
 const ordersValidator = require('../../validators/orders');
 const ordersController = require('../../controllers/orders');
 
 const ordersRouter = express.Router();
 
-ordersRouter.post('/', ordersValidator.create, ordersController.create);
-ordersRouter.delete('/:id', ordersValidator.deleteById, ordersController.deleteById);
+ordersRouter.post('/', ordersValidator.create, adminAuth, ordersController.create);
+ordersRouter.delete('/:id', ordersValidator.deleteById, adminAuth, ordersController.deleteById);
 
 module.exports = ordersRouter;
