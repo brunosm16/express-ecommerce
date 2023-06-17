fca541f7e4132cd2bb5a1b626f9e6eb394399253
brunosm16 <brunoskr23@gmail.com>
2023-06-17 18:16:04 -0300

    refactor: 📝 remove not used errors constants

--- src/errors/errors-constants/bad-request-constants.js
+++
@@ -1,7 +0,0 @@
-const { EntityExistsError, NotFoundError } = require('../errors-types');
-
-const badRequestErrorsInstances = [EntityExistsError, NotFoundError];
-
-const badRequestErrorsTypes = ['TokenExpiredError', 'TypeError'];
-
-module.exports = { badRequestErrorsInstances, badRequestErrorsTypes };
