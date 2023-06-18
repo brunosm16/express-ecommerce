719d109c686da7f8a57bc7bbc96ae9d958b8691a
brunosm16 <brunoskr23@gmail.com>
2023-06-18 20:53:41 -0300

    feat: 🚀 protect address routes

--- src/router/v1/addresses-router.js
+++ src/router/v1/addresses-router.js
@@ -2,15 +2,27 @@ const express = require('express');
 const addressesValidator = require('../../validators/addresses');
 const addressesController = require('../../controllers/addresses');
 const expressHandler = require('../../middlewares/express-handler');
+const { adminAuth, auth } = require('../../middlewares/jwt-auth');
 
 const addressesRouter = express.Router();
 
-addressesRouter.post('/', addressesValidator.create, expressHandler(addressesController.create));
+addressesRouter.post(
+	'/',
+	addressesValidator.create,
+	adminAuth,
+	expressHandler(addressesController.create)
+);
 addressesRouter.delete(
 	'/:id',
 	addressesValidator.remove,
+	auth,
 	expressHandler(addressesController.remove)
 );
-addressesRouter.put('/:id', addressesValidator.update, expressHandler(addressesController.update));
+addressesRouter.put(
+	'/:id',
+	addressesValidator.update,
+	auth,
+	expressHandler(addressesController.update)
+);
 
 module.exports = addressesRouter;
